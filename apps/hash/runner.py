164ef74af826c773dd0570a2a103938949d7c3eb
Adrian Sampson <adrian@radbox.org>
2015-09-12 16:13:24 -0700

    Sort on average rank

--- apps/hash/runner.py
+++ apps/hash/runner.py
@@ -1,7 +1,8 @@
 import subprocess
+import collections
 
 
-def main():
+def get_results():
     results = []
     output = subprocess.check_output(['./hash'])
     for line in output.split('\n'):
@@ -10,10 +11,45 @@ def main():
             name, score = line.split(': ')
             score = float(score)
             results.append((name, score))
-
     results.sort(key=lambda p: p[1])
+    return results
+
+
+def print_results(results):
     for name, score in results:
         print('{}: {}'.format(name, score))
+
+
+def get_ranks(results):
+    ranks = {}
+    for i, (name, _) in enumerate(results):
+        ranks[name] = i
+    return ranks
+
+
+def mean(s):
+    t = 0.0
+    c = 0
+    for v in s:
+        t += v
+        c += 1
+    return t / c
+
+
+def main():
+    rank_lists = collections.defaultdict(list)
+
+    for i in range(10):
+        results = get_results()
+        print_results(results)
+        for name, rank in get_ranks(results).items():
+            rank_lists[name].append(rank)
+        print('---')
+
+    rank_list_pairs = rank_lists.items()
+    rank_list_pairs.sort(key=lambda p: mean(p[1]))
+    for name, ranks in rank_list_pairs:
+        print('{}: {} {}'.format(name, mean(ranks), ranks))
 
 
 if __name__ == '__main__':
