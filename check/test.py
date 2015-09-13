b6b640374d7bbc30e114ea2b32d504bcbbc37756
Cyrus Rashtchian <cyrash@Cyruss-MacBook-Pro.local>
2015-09-12 18:32:59 -0700

    Test two distributions

--- check/test.py
+++ check/test.py
@@ -3,13 +3,11 @@ import numpy
 import sys
 
 def test_normal(data):
-	# numpy.random.seed(12345678)
-	x = numpy.random.normal(0,1,1000)
-	y = numpy.random.normal(0,1,1000)
-	z = numpy.random.normal(1.1,0.9, 1000)
+	norm = numpy.random.normal(0,1,1000)
+	unif = numpy.random.uniform(0,10,1000)
 
-	print(ks_2samp(x, y))
-	print(ks_2samp(x, data))
+	print(ks_2samp(norm, data))
+	print(ks_2samp(unif, data))
 
 if __name__ == '__main__':
     test_normal(numpy.loadtxt(sys.argv[1]))
