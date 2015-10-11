0d03611dc8c35901e0441e8fc76152a315f2fccc
Adrian Sampson <adrian@radbox.org>
2015-10-10 18:02:52 -0700

    Find the winners

---
+++ apps/hash/winner.py
@@ -0,0 +1,23 @@
+import json
+
+
+def winner(results_fn, out_fn):
+    with open(results_fn) as f:
+        results = json.load(f)
+
+    winners = {}
+    for dist, data in results.items():
+        best_hash = None
+        best_score = None
+        for hash, (min, mid, max) in data.items():
+            if best_score is None or mid < best_score:
+                best_score = mid
+                best_hash = hash
+        winners[dist] = best_hash
+
+    with open(out_fn, 'w') as f:
+        json.dump(winners, f, indent=2, sort_keys=True)
+
+
+if __name__ == '__main__':
+    winner('results.json', 'winners.json')
