c4a53e0fdcbbb7abd7626b2ec2822c688a0cd7ac
Adrian Sampson <adrian@radbox.org>
2015-12-07 15:24:05 -0800

    Normality test based on distributions.json

--- check/test.py
+++ check/test.py
@@ -1,13 +1,40 @@
 from scipy.stats import ks_2samp
 import numpy
 import sys
+import json
 
-def test_normal(data):
-	norm = numpy.random.normal(0,1,1000)
-	unif = numpy.random.uniform(0,10,1000)
 
-	print(ks_2samp(norm, data))
-	print(ks_2samp(unif, data))
+def test_normal(data, mean, variance):
+    if len(data) > 10 ** 10:
+        print('that is a lot of data!')
+    norm = numpy.random.normal(mean, variance, len(data))
+    _, p = ks_2samp(norm, data)
+    return p
+
+
+TEST_FUNCTIONS = {
+    'normal': test_normal,
+}
+
+
+def main(data_filename, dists_filename):
+    data = numpy.loadtxt(data_filename)
+
+    with open(dists_filename) as dists_file:
+        dists = json.load(dists_file)
+
+    for dist in dists:
+        try:
+            func = TEST_FUNCTIONS[dist['kind']]
+        except KeyError:
+            print('no test found for', dist['kind'])
+            continue
+
+        args = {k: v for k, v in dist.items() if k not in ('kind', 'name')}
+        score = func(data, **args)
+
+        print(dist['name'], score)
+
 
 if __name__ == '__main__':
-    test_normal(numpy.loadtxt(sys.argv[1]))
+    main(*sys.argv[1:])
