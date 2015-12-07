8b852e83a5185ddf7a949a717c8d9b9ce08ee745
Adrian Sampson <adrian@radbox.org>
2015-12-07 15:38:59 -0800

    Test all the distributions

--- check/test.py
+++ check/test.py
@@ -7,13 +7,53 @@ import json
 def test_normal(data, mean, variance):
     if len(data) > 10 ** 10:
         print('that is a lot of data!')
-    norm = numpy.random.normal(mean, variance, len(data))
-    _, p = ks_2samp(norm, data)
+    synthetic = numpy.random.normal(mean, variance, len(data))
+    _, p = ks_2samp(synthetic, data)
+    return p
+
+
+def test_poisson(data, mean):
+    synthetic = numpy.random.poisson(mean, len(data))
+    _, p = ks_2samp(synthetic, data)
+    return p
+
+
+def test_uniform(data, min, max):
+    synthetic = numpy.random.uniform(min, max, len(data))
+    _, p = ks_2samp(synthetic, data)
+    return p
+
+
+def test_piecewise_constant(data, intervals, weights):
+    total = sum(weights)
+    normalized_weights = [w / total for w in weights]
+
+    synthetic = []
+    for _ in range(len(data)):
+        # Choose a piece.
+        position = numpy.random.ranf()
+        for index, weight in enumerate(normalized_weights):
+            position -= weight
+            if position <= 0.0:
+                break
+        else:
+            assert False
+
+        # Choose the interval from the index.
+        lo, hi = intervals[index], intervals[index + 1]
+
+        # Sample from the range.
+        synthetic.append(numpy.random.uniform(lo, hi))
+
+    _, p = ks_2samp(synthetic, data)
     return p
 
 
 TEST_FUNCTIONS = {
     'normal': test_normal,
+    'poisson': test_poisson,
+    'uniform': test_uniform,
+    'piecewise_constant': test_piecewise_constant,
 }
 
 
