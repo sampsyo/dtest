2f151ee1711e29520ac4343f23e381b860e58bd1
Adrian Sampson <adrian@radbox.org>
2016-02-20 17:03:19 -0800

    Revive plotting script

--- dtest/plot.py
+++ dtest/plot.py
@@ -2,27 +2,30 @@ import boomslang
 import json
 
 
-def plot(results_fn, out_fn):
-    with open(results_fn) as f:
+COLORS = ["red", "green", "blue", "orange"]
+
+
+def plot(quality_fn, out_fn):
+    with open(quality_fn) as f:
         results = json.load(f)
     draw(results, out_fn)
 
 
-def draw(results, out_fn):
+def draw(quality, out_fn):
     bars = []
-    _, first_data = sorted(results.items())[0]
+    _, first_data = sorted(quality.items())[0]
     for j, (hashname, _) in enumerate(sorted(first_data.items())):
         bar = boomslang.Bar()
-        bar.xValues = range(len(results))
+        bar.xValues = range(len(quality))
         bar.yMins = []
         bar.yValues = []
         bar.yMaxes = []
         bar.label = hashname
-        bar.color = "gray"
+        bar.color = COLORS[j]
         bars.append(bar)
 
     labels = []
-    for i, (dist, data) in enumerate(sorted(results.items())):
+    for i, (dist, data) in enumerate(sorted(quality.items())):
         for j, (hashname, (min, mid, max)) in enumerate(sorted(data.items())):
             # bars[j].yMins.append(min)
             bars[j].yValues.append(mid)
@@ -38,10 +41,10 @@ def draw(results, out_fn):
     # clusteredBars.drawErrorBars('y')
 
     plot = boomslang.Plot()
-    # plot.hasLegend()
+    plot.hasLegend()
     plot.add(clusteredBars)
     plot.save(out_fn)
 
 
 if __name__ == '__main__':
-    plot('results.json', 'hash.pdf')
+    plot('model_quality.json', 'hash.pdf')
