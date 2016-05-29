bb9c19de7803dee0942f0abb60dd68ea950a1b12
Adrian Sampson <adrian@radbox.org>
2016-05-29 15:54:45 -0700

    Refactor data conversion script

--- lsh-examples/convert.py
+++ lsh-examples/convert.py
@@ -2,19 +2,26 @@
 
 import sys
 import struct
-import numpy as np
 
-matrix = []
-with open('dataset/glove.6B.100d.txt', 'r') as inf:
-    with open('dataset/glove.6B.100d.dat', 'wb') as ouf:
-        counter = 0
-        for line in inf:
-            row = [float(x) for x in line.split()[1:]]
-            assert len(row) == 100
-            ouf.write(struct.pack('i', len(row)))
-            ouf.write(struct.pack('%sf' % len(row), *row))
-            counter += 1
-            matrix.append(np.array(row, dtype=np.float32))
-            if counter % 10000 == 0:
-                sys.stdout.write('%d points processed...\n' % counter)
-np.save('dataset/glove.6B.100d', np.array(matrix))
+def txt2dat(txtpath):
+    """Convert a text file containing word vectors to a packed data
+    format for GloVe.
+    """
+    assert txtpath.endswith('.txt')
+    datpath = txtpath[:-4] + '.dat'
+
+    with open(txtpath, 'r') as inf:
+        with open(datpath, 'wb') as ouf:
+            counter = 0
+            for line in inf:
+                row = [float(x) for x in line.split()[1:]]
+                assert len(row) == 100
+                ouf.write(struct.pack('i', len(row)))
+                ouf.write(struct.pack('%sf' % len(row), *row))
+                counter += 1
+                if counter % 10000 == 0:
+                    sys.stdout.write('%d points processed...\n' % counter)
+
+
+if __name__ == '__main__':
+    txt2dat('dataset/glove.6B.100d.txt')
