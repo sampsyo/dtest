74aa12d19505a26bd6e815ba817ee2b5c82b35ab
Adrian Sampson <adrian@radbox.org>
2016-05-29 15:47:28 -0700

    Fetch three datasets

--- lsh-examples/prepare-dataset.sh
+++ lsh-examples/prepare-dataset.sh
@@ -2,7 +2,18 @@
 
 mkdir -p dataset
 cd dataset
+
+# 6B: Wikipedia 2014 + Gigaword 5
 curl -O http://nlp.stanford.edu/data/glove.6B.zip
 unzip glove.6B.zip
+
+# 42B: Common Crawl
+curl -O http://nlp.stanford.edu/data/glove.42B.300d.zip
+unzip glove.42B.300d.zip
+
+# 27B: Twitter
+curl -O http://nlp.stanford.edu/data/glove.twitter.27B.zip
+unzip glove.twitter.27B.zip
+
 cd ..
 ./convert.py
